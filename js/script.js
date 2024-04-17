b51cf910544f19e657049a74548eef389b96f713
ryampeguero <ryampeguero@gmail.com>
2024-04-17 12:58:03 +0200

    feat: project scaffolding

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const {createApp} = Vue;
+
+createApp({
+    data(){
+        return {
+            user: "",
+            result: "Ciao qui avviene la stampa",
+            imgName: "./img/01.webp",
+            imgAlt: "Miles Morales"
+        }
+    },
+    
+    methods: {
+        greeting: function(){
+            this.result = this.user
+        }
+    }
+}).mount("#app")
